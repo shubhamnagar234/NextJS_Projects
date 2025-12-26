e63792db321c572ef33770b9d7dab4fe85487668
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-26 08:28:37 +0530

    User Verify, Login and Logout

---
+++ next-auth-app/src/app/api/users/logout/route.ts
@@ -0,0 +1,25 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextResponse } from "next/server";
+
+connect();
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout Successfully!",
+      success: true,
+    });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error: unknown) {
+    return NextResponse.json(
+      { error: error instanceof Error ? error.message : "Unknown error" },
+      { status: 500 }
+    );
+  }
+}
