d9421586a50f90cfbab5f3836f0f4aec58d170f9
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-30 19:44:15 +0530

    Setup Resend Email

---
+++ mystery-message/emails/verification-email.tsx
@@ -0,0 +1,56 @@
+import {
+  Html,
+  Head,
+  Font,
+  Preview,
+  Heading,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification Code</title>
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Preview>Here&apos;s your verification code: </Preview>
+      <Section>
+        <Row>
+          <Heading as="h1">Hello {username}!</Heading>
+          <Text>
+            Thank you for regestering. Please use the following verification
+            code to complete your registeration:
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            If you did not request this code, please ignore this email.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
